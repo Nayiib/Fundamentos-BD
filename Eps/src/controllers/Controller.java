ef30935ea014998f617446fe6f8f99168d4a7a7e
spaezsuarez2 <lamperougelelouch74@gmail.com>
2020-08-09 16:21:44 -0500

    Conexion a la base de datos

---
+++ Eps/src/controllers/Controller.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Controller {
+    protected Connection conexion;
+
+    public Controller() {
+        try {
+            establecerConexion();
+        } catch (SQLException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void establecerConexion() throws SQLException {
+        try {
+            conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/algebra", "postgres", "Tintaculta23");
+            System.out.println("Conexion a la base de datos exitosa " + conexion.getMetaData().getURL());
+        } catch (SQLException exception) {
+            System.out.println("Error en la base de datos ");
+            System.out.println(exception.getMessage());
+        } 
+    }
+
+    public void cerrarConexion() {
+        try {
+            conexion.close();
+        } catch (SQLException ex) {
+            System.out.println("Fallo: " + ex);
+        }
+    }
+    
+    
+    
+    
+}
--- Eps/src/prototipos/Prototipos.java
+++ Eps/src/prototipos/Prototipos.java
@@ -1,23 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prototipos;
 
 /**
  *
  * @author katht
  */
+import controllers.*;
 public class Prototipos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        IniciarSesion Ventana = new IniciarSesion();
+        /*IniciarSesion Ventana = new IniciarSesion();
         Ventana.setVisible(true);
-        Ventana.setLocationRelativeTo(null);
+        Ventana.setLocationRelativeTo(null);*/
+        
+        AfiliadoController test = new AfiliadoController();
+        
+        test.crear(133, "Empresa1", "Donde sea otra vez", 1234);
+        test.cerrarConexion();
     }
     
 }
