4766a27797cb670eb1dea68f6ef214fe55665920
PykyE <miguel35697@hotmail.com>
2020-08-09 16:31:34 -0500

    new

--- EPS/src/Main.java
+++ EPS/src/Main.java
@@ -3,11 +3,15 @@ import View.EliminarCita;
 import View.RegistrarDiagnostico;
 import View.AgendaMedico;
 import View.AgendaMedico2;
+import View.AgregarEspMedico;
+import View.AsignarCita;
+import View.ConsulCita;
+import View.RegistrarMedico;
+import View.ResulConsulCita;
 
 public class Main {
     
     public static void main(String[] args){
-        
         
         EliminarCita asd = new EliminarCita();
         
