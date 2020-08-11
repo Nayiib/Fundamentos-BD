7c15f67b1808dc42e94b98a8f3fc6fce9bc33dff
spaezsuarez2 <lamperougelelouch74@gmail.com>
2020-08-11 08:07:34 -0500

    Autenticacion de usuario (hacer pruebas)

--- Eps/src/controllers/Controller.java
+++ Eps/src/controllers/Controller.java
@@ -3,30 +3,38 @@ package controllers;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.ResultSet;
 import ignore.Keys;
-
+import java.sql.PreparedStatement;
 
 public class Controller {
-    protected Connection conexion;
 
-    public Controller() {
+    protected Connection conexion;
+    private static Controller controlador;
+
+    protected Controller() {
         try {
             establecerConexion();
         } catch (SQLException ex) {
-            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex.getMessage());
         }
+    }
+
+    public static Controller getReference() {
+        if (controlador == null) {
+            controlador = new Controller();
+        }
+        return controlador;
     }
 
     public void establecerConexion() throws SQLException {
         try {
-            conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eps", Keys.user,Keys.password);
+            conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/eps", Keys.user, Keys.password);
             System.out.println("Conexion a la base de datos exitosa " + conexion.getMetaData().getURL());
         } catch (SQLException exception) {
             System.out.println("Error en la base de datos ");
             System.out.println(exception.getMessage());
-        } 
+        }
     }
 
     public void cerrarConexion() {
@@ -36,8 +44,29 @@ public class Controller {
             System.out.println("Fallo: " + ex);
         }
     }
-    
-    
-    
-    
+
+    public long getUser(String tabla, String tipo, long id) {
+        ResultSet user = null;
+        long comparacion = 0;
+
+        try {
+            String consulta = "SELECT * FROM " + tabla + " WHERE k_tipodocumento = ? AND k_numerodocumento = ?;";
+            PreparedStatement st = conexion.prepareStatement(consulta);
+            st.setString(1, tipo);
+            st.setLong(2, id);
+
+            user = st.executeQuery();
+            while (user.next()) {
+                comparacion = user.getLong(2);
+                System.out.println(user.getStatement().toString());
+                System.out.println(comparacion);
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return comparacion;
+    }
+
 }
--- Eps/src/prototipos/IniciarSesion.java
+++ Eps/src/prototipos/IniciarSesion.java
@@ -2,6 +2,7 @@ package prototipos;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Dimension;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,91 +16,110 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JOptionPane;
 
-import controllers.AdministradorController;
+import controllers.Controller;
+import java.util.Locale;
 
+public class IniciarSesion extends JFrame {
 
-public class IniciarSesion extends JFrame{
-    
-    public JPanel panel;
-    private AdministradorController administrador;
-    
-    public IniciarSesion(){
+    private JPanel panel;
+    private Controller controlador;
+
+    public IniciarSesion() {
         initCompo();
         mostrar();
-        administrador = AdministradorController.getReference();
+        controlador = Controller.getReference();
+
     }
-    
-    public void initCompo(){
-        setSize(600,320); 
+
+    public void initCompo() {
+        setSize(600, 320);
         setTitle("Iniciar Sesión");
+        setResizable(false);
         panel = new JPanel();
         panel.setLayout(null);
         this.getContentPane().add(panel);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
-    public void mostrar(){
+
+    public void mostrar() {
         JLabel Titulo = new JLabel("Iniciar Sesión", SwingConstants.CENTER);
         Titulo.setBounds(0, 10, 600, 30);
-        Titulo.setFont(new Font("Serif", Font.BOLD, 22)); 
+        Titulo.setFont(new Font("Serif", Font.BOLD, 22));
         Titulo.setForeground(new Color(21, 67, 96));
         panel.add(Titulo);
-        
+
         JLabel TipoD = new JLabel("Tipo");
         TipoD.setBounds(100, 90, 300, 30);
-        TipoD.setFont(new Font("Serif", Font.BOLD, 14)); 
+        TipoD.setFont(new Font("Serif", Font.BOLD, 14));
         panel.add(TipoD);
-        
+
+        JLabel textTipoUsuario = new JLabel("¿Quien esta ingresando?");
+        textTipoUsuario.setBounds(100, 120, 700, 30);
+        textTipoUsuario.setFont(new Font("Serif", Font.BOLD, 14));
+        panel.add(textTipoUsuario);
+
+        JComboBox<String> TipoUsuario = new JComboBox<>();
+        TipoUsuario.addItem("Medico");
+        TipoUsuario.addItem("Afiliado/Beneficiario");
+        TipoUsuario.setBounds(305, 125, 200, 20);
+        panel.add(TipoUsuario);
+
         JComboBox<String> MTipo = new JComboBox<>();
-        MTipo.addItem("Medico");
-        MTipo.addItem("Afiliado");
+        MTipo.addItem("Cedula de ciudadania");
+        MTipo.addItem("Cedula de extrangeria");
+        MTipo.addItem("Pasaporte");
+        MTipo.addItem("Registro civil");
         MTipo.setBounds(305, 95, 200, 20);
         MTipo.setBackground(Color.white);
         panel.add(MTipo);
-        
+
         JLabel ID = new JLabel("Número identificación *");
-        ID.setBounds(100, 120, 300, 30);
-        ID.setFont(new Font("Serif", Font.BOLD, 14)); 
+        ID.setBounds(100, 150, 300, 30);
+        ID.setFont(new Font("Serif", Font.BOLD, 14));
         panel.add(ID);
-        
+
         JTextField CID = new JTextField();
-        CID.setBounds(305, 125, 200, 20);
+        CID.setBounds(305, 155, 200, 20);
         panel.add(CID);
-        
+
         JButton ISesion = new JButton("Iniciar Sesión");
-        ISesion.setBounds(200, 175, 150, 30);
+        ISesion.setSize(new Dimension(150, 30));
+        ISesion.setLocation(220, 200);
+        //ISesion.setBounds(200, 200, 150, 30);
         panel.add(ISesion);
-        
+
         ISesion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String Id = CID.getText();
-                String tipo = String.valueOf(MTipo.getSelectedItem());
-                
-                if(tipo.equals("Medico")){
-                    if(administrador.getMedico(Id) != null){
-                        InterfazAB AB = new InterfazAB();
-                        AB.setVisible(true);
-                        AB.setLocationRelativeTo(null);
-                    }else{
-                        JOptionPane.showMessageDialog(null, "Error Autenticacion", "Error", JOptionPane.ERROR_MESSAGE);
-                    }
-                }
-                
-                if(tipo.equals("Afiliado")){
-                    if(administrador.getAfiliado(Id) != null){
-                        InterfazAB AB = new InterfazAB();
-                        AB.setVisible(true);
-                        AB.setLocationRelativeTo(null);
-                    }else{
-                        JOptionPane.showMessageDialog(null, "Error Autenticacion", "Error", JOptionPane.ERROR_MESSAGE);
-                    }
-                }
-                
-            }	
-        });
- 
-    }
+                String tipoUsuario = String.valueOf(TipoUsuario.getSelectedItem());
+                String tipoDocumento = String.valueOf(MTipo.getSelectedItem());
 
+                if (tipoUsuario.equals("Medico")) {
+                    if (controlador.getUser("medico", tipoDocumento, Long.parseLong(Id)) != 0) {
+                        IniciarSesion.this.setVisible(false);
+                        InterfazAB AB = new InterfazAB();
+                        AB.setVisible(true);
+                        AB.setLocationRelativeTo(null);
+                    }else {
+                        JOptionPane.showMessageDialog(null, "Por favor asegurese que sus datos esten bien escritos", "Error Autenticacion", JOptionPane.ERROR_MESSAGE);
+                    }
+                    
+                } else {
+                    if (controlador.getUser("afiliado_beneficiario", tipoDocumento, Long.parseLong(Id)) != 0) {
+                        IniciarSesion.this.setVisible(false);
+                        InterfazMedico medico = new InterfazMedico();
+                        medico.setVisible(true);
+                        medico.setLocationRelativeTo(null);
+
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Por favor asegurese que sus datos esten bien escritos", "Error Autenticacion", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+
+            }
+        });
+
+    }
 
 }
