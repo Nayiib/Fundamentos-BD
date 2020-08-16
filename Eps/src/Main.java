e0bbedcd81fd4e2c68e294be62750bcf9aca5642
spaezsuarez2 <lamperougelelouch74@gmail.com>
2020-08-16 17:23:44 -0500

    guardo cambios

--- Eps/src/Main.java
+++ Eps/src/Main.java
@@ -1,16 +1,12 @@
 import Gui.*;
-import Gui.Admin.RegistrarMedico;
-import Gui.AfiliadoBeneficiario.RegistrarAB;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*IniciarSesion Ventana = new IniciarSesion();
+        IniciarSesion Ventana = new IniciarSesion();
         Ventana.setVisible(true);
-        Ventana.setLocationRelativeTo(null);*/
-        RegistrarAB test = new RegistrarAB();
-        test.setLocationRelativeTo(null);
-        test.setVisible(true);
+        Ventana.setLocationRelativeTo(null);
+        
         
          
     }
