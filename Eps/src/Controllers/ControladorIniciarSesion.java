fa513eadcbb29e75940dc57f5600fb68da4f12b5
dmartinez2 <daniel.martinez@liftit.com>
2020-08-20 11:43:13 -0500

    lazy load

--- Eps/src/Controllers/ControladorIniciarSesion.java
+++ Eps/src/Controllers/ControladorIniciarSesion.java
@@ -10,10 +10,6 @@ public class ControladorIniciarSesion {
 
     private DAO dao;
 
-    public ControladorIniciarSesion(){
-        dao = DAO.getReference();
-    }
-
     public void iniciarApp(){
         IniciarSesion Ventana = new IniciarSesion(this);
         Ventana.setVisible(true);
@@ -26,9 +22,13 @@ public class ControladorIniciarSesion {
 
     public boolean obtenerUsuario (String tabla, String tipo, long id){
         try {
+            dao = DAO.getReference();
+            dao.establecerConexion();
             return dao.getUser(tabla, tipo, id);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        } finally {
+            dao.cerrarConexion();
         }
         return false;
     }
